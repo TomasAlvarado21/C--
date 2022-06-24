46dc2148189a55483e9757a68f626fd348b8edd6
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-06-24 11:43:40 -0400

    pc casi lista

--- T5/p1.cpp
+++ T5/p1.cpp
@@ -3,22 +3,51 @@
 #include <string>
 using namespace std;
 
-// Function to find the maximum pair of numbers in an array that
+// Function to find the all the pairs of numbers in an array that
 // the maximum common divisor of the two numbers > 1
 void findMaxPair(int arr[], int n){
-    // we need to reorder the array in order to find the maximum
-    // common divisor
-    int temp;
+    // Sort the array
+    sort(arr, arr+n);
+    reverse(arr, arr+n);
+    // Initialize the result
+    int result = 0;
+    // Initialize the two pointers
+    int i = 0, j = n-1;
+    // Traverse the array
+    while (i < n){
+        while(i < j){
+            // If the maximum common divisor of the two numbers is > 1
+            if(__gcd(arr[i], 2 * arr[j]) > 1){
+                // Update the result
+                result++;
+                // Move the pointer to the next element
+                j--;
+            }
+            // If the maximum common divisor of the two numbers is 1
+            else{
+                // Move the pointer to the next element
+                j--;
+            }
+        }
+        i++;
+        j = n-1;
+    }
+    // Print the result
+    cout << result << endl;
 }
 
-// Function to find the maximum common divisor of a number and the double of the second number
-int findMaxCommonDivisor(int a, int b){
-    // we need to find the maximum common divisor of a and 2*b
-    int temp;
-    
-    if (a == 0 || b == 0)
-        return 0;
-
+int main(){
+    int a;
+    int n;
+    cin >> a;
+    for (int i = 0; i < a; ++i){
+        cin >> n;
+        int arr[n];
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> arr[j];
+        }
+        findMaxPair(arr, n);
+    }
 }
-
 
