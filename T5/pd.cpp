2fa948d117bca910a26165b10f19bcb90e6f302e
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-07-01 16:05:33 -0400

    bla

--- T5/pd.cpp
+++ T5/pd.cpp
@@ -3,30 +3,26 @@
 #include <string>
 using namespace std;
 #include <algorithm>
-
 // we need a function that firts recives the number of cases, then the next input n the number of univeristies and students
 // the next n inputs are the university of the i-th student
 // the next n inputs are the hability of the i-th student
 // k is the number max number of students that can be in a equipment for each university, this number increases with the number of students in the university
 // the output is the sum of all the students that can be in a equipment for each university, and this output is the power of the region that includes al the universities
+// we need a vector with vectors, that represents the universities and the students
 
-//the array is a vector with vectors, that represents the universities and the students
-void max_power(int arr[]){
+void max_power(vector<vector<int>> arr){
     int n = arr.size();
-    int arr2[n];
-    int k = 1;
-    
+    int k = arr[n].back();
+    arr[n].pop_back();
     int result = 0;
     for (int i = 0; i < n; i++){
-        copy(arr.begin(), arr.end(), arr2);
-        sort(arr2, arr2 + n);
-        while (arr2[i].size() % k != 0){
-            arr2[i].pop_back();
+        sort(arr[i].begin(), arr[i].end());
+        while (arr[i].size() % k != 0){
+            arr[i].pop_back();
         }
         for (int j = 0; j < k; j++){
-            result += arr2[i][j];
+            result += arr[i][j];
         }
-        
     }
     cout << result << endl;
 }
@@ -37,7 +33,7 @@ int main(){
     for (int i = 0; i < n; i++){
         int n;
         cin >> n;
-        vector<int> arr[n];
+        vector<vector<int>> arr(n);
         for (int j = 0; j < n; j++){
             int a;
             cin >> a;
@@ -48,5 +44,4 @@ int main(){
         arr[n].push_back(k);
         max_power(arr);
     }
-    return 0;
 }
